4d7956ceb76af44e591704f5e2085ea18a9c11eb
upkl <2737384+upkl@users.noreply.github.com>
2023-08-20 16:59:28 +0200

    added Editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
 
   def user_params
