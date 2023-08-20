8460296f7611f6b8caee4be93d4024c0edb622d1
upkl <2737384+upkl@users.noreply.github.com>
2023-08-20 16:05:27 +0200

    created html form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @User
+      redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
     end
